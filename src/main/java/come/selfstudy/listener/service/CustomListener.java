086a76dfb01de31c59992402facab257706ef7c8
psaadat <parastoo.sa.1982@gmail.com>
2024-05-17 13:00:43 -0400

    no need

--- src/main/java/come/selfstudy/listener/service/CustomListener.java
+++ src/main/java/come/selfstudy/listener/service/CustomListener.java
@@ -6,7 +6,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class CustomListener implements ApplicationListener<ContextRefreshedEvent> {
     private static final Logger logger = LogManager.getLogger(CustomListener.class);
     @Override
